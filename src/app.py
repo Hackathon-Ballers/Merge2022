6871093b526a56ef68f755e189c03f728c9bfdd1
Joey Marcinowski <jmarcinowski@hillers.org>
2022-07-23 15:05:12 -0400

    frwhui

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from cachelib import RedisCache
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_session import Session
 from user import User
+from post import Post
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -23,25 +24,25 @@ app.secret_key = 'super secret key'
 @app.route('/')
 def index():
     global posts
-    if not session.get('username'): return redirect("/login")
-    return render_template('index.html', current_user=session['username'], posts=posts[::-1])
+    if not session.get('user'): return redirect("/login")
+    return render_template('index.html', current_user=session['user'], posts=posts[::-1])
 
 @app.route("/new", methods=["GET", "POST"])
 def new():
-    if not session.get('username'): return redirect("/login")
+    if not session.get('user'): return redirect("/login")
     if request.method == "POST":
         title = request.form.get("title")
         content = request.form.get("content")
         if not title or not content: return redirect("/new")
-        posts.append({"title": title, "content": content, "author": session['username']})
+        posts.append(Post(title=title, content=content, author=session['user'].username))
         return redirect("/")
-    return render_template('new.html', current_user=session['username'])
+    return render_template('new.html', current_user=session['user'])
 
 @app.route('/user/<viewed_username>')
 def user(viewed_username):
     if not viewed_username: return redirect("/")
-    if not session.get('username'): return redirect("/login")
-    return render_template('user.html', current_user=session['username'], viewed_username=viewed_username)
+    if not session.get('user'): return redirect("/login")
+    return render_template('user.html', current_user=session['user'], viewed_username=viewed_username)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -50,7 +51,7 @@ def login():
         password = request.form['password']
         for user in users:
             if user.username != username or user.password != password: continue
-            session['username'] = username
+            session['user'] = user
             return redirect("/")
         else:
             return redirect("/login")
@@ -61,6 +62,11 @@ def logout():
     session.pop('username', None)
     print(session)
     return redirect("/login")
+
+@app.route('/test')
+def test():
+    print(session['user'])
+    return f"hello {session['user'].username}"
 
 @app.errorhandler(404)
 def not_found(error):
--- src/post.py
+++ src/post.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 @dataclass
 class Post:
-    id: int
     title: str
     content: str
     author: str
     date: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    belongs_to: int
+    id: int = 0
+    belongs_to:any = None
